9f6bf271f9cfd0aa702a34e3fba83bdb238f7067
galargh <piotr.galar@gmail.com>
2023-09-20 15:53:29 +0100

    fix: filecoin-project monitoring

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ variable "github_app_webhook_secret" {}
 variable "organization_allow_list" {
   description = "List of org names allowed to use the github app"
   type        = list(string)
-  default     = ["filecon-project", "filecoin-shipyard", "ipfs", "ipfs-examples", "ipfs-shipyard", "ipld", "ipni", "libp2p", "multiformats", "pl-strflt", "plprobelab", "testground", "quic-go"]
+  default     = ["filecoin-project", "filecoin-shipyard", "ipfs", "ipfs-examples", "ipfs-shipyard", "ipld", "ipni", "libp2p", "multiformats", "pl-strflt", "plprobelab", "testground", "quic-go"]
 }
 
 variable "log_type" {
